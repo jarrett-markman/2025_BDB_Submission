fd3c9e6baa8331834d746b47306266406e1b4eae
Jarrett Markman <97311712+jarrett-markman@users.noreply.github.com>
2025-01-05 22:26:21 -0500

    Update app.R with new changes

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 # Find all plays with no motion from the datasets
-# read_tracking <- readRDS("tracking.rds") # Read in read_tracking function from code.R
+# read_tracking <- readRDS("tracking.rds")
 # Read in all tracking data
 # tracking <- bind_rows(map(1:9, read_tracking))
 # Find the unique games in which no motion occurs
@@ -19,7 +19,6 @@ library(DT)
 #   # Initialize the no motion col. for plays with no man_in_motion event
 #   mutate(c1 = 1, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0) 
 #saveRDS(no_motions, "no_motions.rds")
-# Save no_motions as a .rds object so that the data takes up less storage
 no_motions <- readRDS("no_motions.rds")
 # Read in plays .csv
 plays <- read_csv("plays.csv") %>% # Summarise cols. from plays 
@@ -28,10 +27,9 @@ plays <- read_csv("plays.csv") %>% # Summarise cols. from plays
 # Read in results .rds file
 res <- readRDS("results.rds")
 # Create motions df with the unique motions on a play
-# Gets the unique number of motions based on whatever motion(s) occur on that play
 motions <- res %>% 
   group_by(gameId, playId) %>%
-  mutate( 
+  mutate(
     c1 = ifelse(cluster == 1, 1, 0), 
     c2 = ifelse(cluster == 2, 1, 0),
     c3 = ifelse(cluster == 3, 1, 0),
@@ -46,35 +44,36 @@ motions <- res %>%
 motions <- bind_rows(motions, no_motions)
 # Change colnames
 motions <- motions %>%
-  rename( # Label motion types
+  rename(
     "Motion across the field" = c6,
-    "Quarter-field motions" = c4,
-    "Half-field motions" = c3,
-    "Quick shifts" = c2,
+    "Quarter-field motion" = c4,
+    "Half-field motion" = c3,
+    "Quick shift" = c2,
     "No motion" = c1,
-    "Slow shifts" = c5
+    "Slow shift" = c5
   )
 # Join plays data frame on motions
 motions_w_coverages <- motions %>%
   left_join(plays, by = c("gameId", "playId")) %>%
   mutate(
     pffCoverage = paste(pff_passCoverage, pff_manZone, sep = ", "),
-    # Use gsub to change patter for manZone = Other, and NA plays
+    # Use gsub to fix different pff charted coverage
     pffCoverage = gsub(", Other", "", pffCoverage),
-    pffCoverage = gsub("NA, NA", "Uncharted", pffCoverage)
+    pffCoverage = gsub("NA, NA", "Uncharted", pffCoverage),
+    pffCoverage = gsub("2-Man, Man", "Cover-2, Man", pffCoverage)
   )
 motions_w_coverages %>%
   group_by(offenseFormation, pffCoverage, playType,
-           `No motion`, `Motion across the field`, `Quarter-field motions`, `Half-field motions`,
-           `Quick shifts`, `Slow shifts`) %>%
+           `No motion`, `Motion across the field`, `Quarter-field motion`, `Half-field motion`,
+           `Quick shift`, `Slow shift`) %>%
   summarise(plays = n(), 
             avg_epa = round(mean(expectedPointsAdded), digits = 2)) %>% 
   ungroup() %>%
   distinct() -> df_by_play_type
 motions_w_coverages %>%
   group_by(offenseFormation, pffCoverage,
-           `No motion`, `Motion across the field`, `Quarter-field motions`, `Half-field motions`,
-           `Quick shifts`, `Slow shifts`) %>%
+           `No motion`, `Motion across the field`, `Quarter-field motion`, `Half-field motion`,
+           `Quick shift`, `Slow shift`) %>%
   mutate(rush = ifelse(playType == "rush", 1, 0),
          rushes = sum(rush)) %>%
   summarise(plays = n(), 
@@ -96,11 +95,11 @@ ui <- dashboardPage(
   dashboardSidebar(
     # Sidebar with filters (unchanged)
     checkboxGroupInput("motions", "Select Motion Types:", 
-                       choices = c("No motion", "Motion across the field", "Quarter-field motions", "Half-field motions", "Quick shifts", "Slow shifts")),
+                       choices = c("No motion", "Motion across the field", "Quarter-field motion", "Half-field motion", "Quick shift", "Slow shift")),
     selectInput("formation", "Select Offensive Formation:", 
                 choices = na.omit(formations_by_freq)),
     selectInput("playType", "Guess Play Type:", 
-                choices = c("probabilities", "rush", "pass"))
+                choices = c("default (no guess)", "rush", "pass"))
   ),
   dashboardBody(
     # Add tabs for Main Page and References
@@ -135,26 +134,27 @@ server <- function(input, output, session) {
     vec <- input$motions
     m_inputs <- data.frame(
       c1 = ifelse("No motion" %in% vec, 1, 0),
-      c2 = ifelse("Quick shifts" %in% vec, 1, 0),
-      c3 = ifelse("Half-field motions" %in% vec, 1, 0),
-      c4 = ifelse("Quarter-field motions" %in% vec, 1, 0),
-      c5 = ifelse("Slow shifts" %in% vec, 1, 0),
+      c2 = ifelse("Quick shift" %in% vec, 1, 0),
+      c3 = ifelse("Half-field motion" %in% vec, 1, 0),
+      c4 = ifelse("Quarter-field motion" %in% vec, 1, 0),
+      c5 = ifelse("Slow shift" %in% vec, 1, 0),
       c6 = ifelse("Motion across the field" %in% vec, 1, 0)
     )
     # Account for cases for playType display
-    if (input$playType == "probabilities") {
+    if (input$playType == "default (no guess)") {
       filtered_df <- df %>%
         filter(offenseFormation == input$formation & 
-                 `Motion across the field` == m_inputs$c6 & `Quarter-field motions` == m_inputs$c4 & 
-                 `Half-field motions` == m_inputs$c3 & `Quick shifts` == m_inputs$c2 & 
-                 `No motion` == m_inputs$c1 & `Slow shifts` == m_inputs$c5) %>%
+                 `Motion across the field` == m_inputs$c6 & `Quarter-field motion` == m_inputs$c4 & 
+                 `Half-field motion` == m_inputs$c3 & `Quick shift` == m_inputs$c2 & 
+                 `No motion` == m_inputs$c1 & `Slow shift` == m_inputs$c5) %>%
+        arrange(avg_epa) %>% 
         select("Coverage (via PFF)" = pffCoverage, Plays = plays, "Rush Frequency" = r_freq, "EPA/Play" = avg_epa, "EPA/Play if Rush" = epa_if_rush, "EPA/Play if Pass" = epa_if_pass)
     } else {
       filtered_df <- df_by_play_type %>%
         filter(offenseFormation == input$formation & playType == input$playType & 
-                 `Motion across the field` == m_inputs$c6 & `Quarter-field motions` == m_inputs$c4 & 
-                 `Half-field motions` == m_inputs$c3 & `Quick shifts` == m_inputs$c2 & 
-                 `No motion` == m_inputs$c1 & `Slow shifts` == m_inputs$c5) %>%
+                 `Motion across the field` == m_inputs$c6 & `Quarter-field motion` == m_inputs$c4 & 
+                 `Half-field motion` == m_inputs$c3 & `Quick shift` == m_inputs$c2 & 
+                 `No motion` == m_inputs$c1 & `Slow shift` == m_inputs$c5) %>%
         arrange(avg_epa) %>%
         select("Coverage (via PFF)" = pffCoverage, Plays = plays, "Average EPA" = avg_epa)
     }
